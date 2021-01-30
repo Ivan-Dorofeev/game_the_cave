93ce4cf837d5c77df3efa6d5faa01364c1c94dde
IvanDorofeev <axxel123@rambler.ru>
2021-01-30 12:42:38 +0300

    Правки

--- snippets/ticket_cons.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import sys
-from pprint import pprint
-
-parser = argparse.ArgumentParser(description='Ticket to End_Ticket')
-parser.add_argument('input_file', type=str, help='Input file for clear Ticket')
-parser.add_argument('output_file', type=str, help='Output file for end Ticket')
-args = parser.parse_args()
-print(args.input_file)
